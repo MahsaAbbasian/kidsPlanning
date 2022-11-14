58671c94a1600701765ece42a70bce71859b5741
Moeinian <sadegh.moeinian@midroc.se>
2022-11-14 21:38:10 +0100

    draft version

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./Pages/Home/Home";
+import Profile from "./Pages/Profile/Profile";
+
+function App() {
+  return (
+    <div>
+      <Home />
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
